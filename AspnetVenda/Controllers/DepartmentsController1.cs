580c3d0920a59a0d75e30927f007f822e40173f3
Rennylson Lemos <rennylsonlemos4321@gmail.com>
2022-02-15 20:33:27 -0300

    introducao ao projeto

---
+++ AspnetVenda/Controllers/DepartmentsController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetVenda.Models;
+
+
+namespace AspnetVenda.Controllers
+{
+    public class DepartmentsController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "eletronics" });
+            list.Add(new Department { Id = 2, Name = "fashion" });
+
+
+            return View(list);
+        }
+    }
+}
